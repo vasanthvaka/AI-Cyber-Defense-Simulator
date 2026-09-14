958a517e83c4ea04646b42805640a5e492847d95
Vasanth Vaka <vs.vaka22@gmail.com>
2026-09-14 10:52:56 +0530

    Add distributed brute-force simulation

--- simulator/brute_force.py
+++ simulator/brute_force.py
@@ -10,8 +10,8 @@ normal_ips = [
     "192.168.1.11",
     "192.168.1.12"
 ]
-
 attacker_ip = "10.0.0.50"
+distributed_attacker_ips = [ "10.0.0.51", "10.0.0.52", "10.0.0.53", "10.0.0.54", "10.0.0.55" ]
 
 
 def create_login_event(username, ip, status):
@@ -46,12 +46,18 @@ def generate_brute_force():
         ip=attacker_ip,
         status="FAILED"
     )
-
+def generate_distributed_brute_force(): 
+    ip = random.choice(distributed_attacker_ips) 
+    return create_login_event( 
+        username="admin", 
+        ip=ip, 
+        status="FAILED" 
+    )
 
 for i in range(20):
 
     if 7 <= i <= 14:
-        event = generate_brute_force()
+        event = generate_distributed_brute_force()
     else:
         event = generate_normal_login()
 
